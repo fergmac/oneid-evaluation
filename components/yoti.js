4f0340b8eceab3bd6c921c68d0837c37423756b8
rsubra13 <ramkisastra@gmail.com>
2022-04-19 16:29:31 -0700

    Working yoti file

--- components/yoti.js
+++ components/yoti.js
@@ -1,73 +1,59 @@
-import { useLayoutEffect, useEffect } from "react";
+import {useLayoutEffect, useEffect} from 'react';
 import Image from 'next/image';
 
 function YotiProvider() {
-
-  const url = 'https://api.yoti.com/idverify/v1/web/index.html?sessionID=fc6bd482-99fa-4ee2-b502-e4a396604f23&sessionToken=1c3e8ea4-0e05-4311-96d7-6a872af97595'
-  const oldUrl = "https://api.yoti.com/sandbox/idverify/v1/web/index.html"
-    useEffect(() =>{
-        const yoti=window.Yoti.Share.init({
-          'elements': [{
-            'clientSdkId': "28d8660a-ac5c-4111-9d9a-2eb3e24a2c3b",
-            // 'scenarioId':"xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-            'domId': "yoti-button",
-            displayLearnMoreLink: false,
-
-          }]
-        })
-
-        return yoti.destroy
-      }, [])
-
-        // const iframe = document.getElementById('iframeId').contentWindow;
-        // const origin = 'https://api.yoti.com';
-        // window.addEventListener(
-        //     'message',
-        //     event => {
-        //         if (event.data.eventType === 'STARTED' && event.origin === origin) {
-        //             iframe.postMessage({
-        //                 eventType: 'INIT_SESSION',
-        //                 sessionID: 'someSessionId',
-        //                 sessionToken: 'someSessionToken',
-        //             },
-        //                 origin
-        //             );
-        //         }
-        //     }
-        // );
-        // window.addEventListener('message',
-        //     function (event) {
-        //         console.log('Message received', event.data);
-
-        //         if (event.data.eventType === 'SUCCESS') {
-        //             // Act upon success
-        //         } else if (event.data.eventType === 'ERROR') {
-        //             // Act upon error
-        //             const errorCode = event.data.eventCode;
-        //         }
-        //     }
-        // );
+  useEffect(() => {
+    const iframe = document.getElementById ('iframeId').contentWindow;
+    const origin = 'https://api.yoti.com/';
+    window.addEventListener('message', event => {
+      const sessionId ='a4202495-9fae-42ea-999d-7e50aa5cdfb8'
+      const sessionToken='feb9ae99-ee69-4b4e-af18-151e86c1755f'
+      if (event.data.eventType === 'STARTED' && event.origin === origin) {
+        iframe.postMessage (
+          {
+            eventType: 'INIT_SESSION',
+            sessionID: {sessionID: sessionId},
+            sessionToken: {sessionToken: sessionToken},
+          },
+          origin
+        );
+      }
+    });
+    window.addEventListener ('message', function (event) {
+      console.log ('Message received', event.data);
+      if (event.data.eventType === 'SUCCESS') {
+        // Act upon success
+      } else if (event.data.eventType === 'ERROR') {
+        // Act upon error
+        const errorCode = event.data.eventCode;
+      }
+    });
+  });
   return (
-        <div className="section">
-            <Image className="logo" width="100" height="50" src="/logo_yoti.png" alt="OneID provider logo" />
-            <div>
-          <iframe
-                src={url}
-                allow="camera"
-                width="100%"
-                height="100%"
-                style={{
-                    "height": "605px;",
-                    "width": "100%;",
-                    "border": "none"
-                }}
-                id="iframeId">
-                </iframe>
-                <div id="yoti-button"></div>
-            </div>
-        </div>
-    );
-
+    <div className="section">
+      <Image
+        className="logo"
+        width="50"
+        height="50"
+        src="/logo_yoti.png"
+        alt="OneID provider logo"
+      />
+      <div>
+        <iframe
+          src={process.env.YOTI_IFRAME_URL}
+          allow="camera"
+          width="100%"
+          height="100%"
+          style={{
+            height: '605px;',
+            width: '100%;',
+            border: 'none',
+          }}
+          id="iframeId"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default YotiProvider;
