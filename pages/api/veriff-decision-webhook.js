982a36c2912bc58a98d0f29be4d5018468ee3f44
Fergus MacConnell <f.macconnell@gmail.com>
2022-04-20 13:27:38 -0700

    Format request data.

--- pages/api/veriff-decision-webhook.js
+++ pages/api/veriff-decision-webhook.js
@@ -1,5 +1,15 @@
 async function handler(req, res) {
     console.log("Decision Webhook Body: ", req.body)
+
+    const data = {
+        "user_id": "",
+        "session_id": "",
+        "response": req.body,
+        "provider": "",
+        "start_time": "",
+        "stop_time": ""
+    }
+    
     try {
         const response = await fetch("https://dcwsy6m8yg.execute-api.ca-central-1.amazonaws.com/default/one_id_testing_responses", {
             method: "PATCH",
@@ -7,7 +17,7 @@ async function handler(req, res) {
                 "Content-Type": "application/json",
                 "x-api-key": process.env.API_KEY,
             },
-            body: req.body
+            body: data
         });
         res.status(response.status).json({msg: "Decision Webhook Success"});
     } catch (error) {
