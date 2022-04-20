b757dab8afbf37afedd653b3ba796130e2f6f424
Fergus MacConnell <f.macconnell@gmail.com>
2022-04-20 16:59:56 -0700

    Initial vouched setup. Add onInit and onDone callback methods.

--- components/vouched.js
+++ components/vouched.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import Image from 'next/image';
 
 
-function Vouched() {
+function VouchedProvider() {
+    const [showVouchedRoot, setShowVouchedRoot] = useState(false);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
+        const userData = localStorage.getItem("userData");
+
         var vouched = Vouched({
-            appId: "Vu#UQ-wfriGb!85W0cZEEwGS#7.s~n",
+            appId: "~qjjNu47bOm6zoYO*2GFY-V#2sYG#y",
             // your webhook for POST verification processing
             callbackURL: 'https://oneid-evaluation-bckyzddv6-fergmac.vercel.app/api/vouched-webhook',
             // mobile handoff
@@ -17,26 +20,78 @@ function Vouched() {
             theme: {
                 name: 'avant',
             },
+            onInit: ({ token, job }) => {
+
+                data = {
+                    "user_id": userData.id,
+                    "session_id": job.token,
+                    "response": "",
+                    "provider": "vouched",
+                    "session_start_time": "test-start-time",
+                    "session_end_time": "",
+                    "session_response_time": ""
+                }
+
+                console.log("Vouched Session onInit");
+
+                fetch('api/vouched-events-webhook', {
+                    method: 'POST',
+                    body: data
+                })
+                    .then((res) => {
+                        console.log("response post fetch", res)
+                    }).catch((err) => console.log("Error: ", err));
+                
+            },
+            onDone: ({ token, obj }) => {
+                data = {
+                    "user_id": userData.id,
+                    "session_id": job.token,
+                    "response": "",
+                    "provider": "vouched",
+                    "session_start_time": "",
+                    "session_end_time": "test-end-time",
+                    "session_response_time": ""
+                }
+
+                console.log("Vouched Session onDone");
+
+                fetch('api/vouched-events-webhook', {
+                    method: 'POST',
+                    body: data
+                })
+                    .then((res) => {
+                        console.log("response post fetch", res)
+                    }).catch((err) => console.log("Error: ", err));
+                
+            }
         });
         vouched.mount("#vouched-root");
     });
 
-    // let vouchedOpen = false;
-    // const toggleVouched = () => {
-    //     vouchedOpen = !vouchedOpen
-    //     document.getElementById("vouched-root").style.visibility = vouchedOpen ? "visible" : "hidden";
-    // }
+    // useEffect(() => {
+    //     window.addEventListener('message', (event) => {
+    //         if (event.data.eventType === 'STARTED') {
+    //             console.log("Vouched Session Message - Started.");
+    //         }
+    //         console.log("Event Data: ", event)
+    //     });
+    // })
+
+    const toggleVouched = () => {
+        setShowVouchedRoot(true);
+    }
 
     return (
         <div className="section">
             <Image className="logo" width="300" height="200" src="/logo_vouched.png" alt="OneID provider logo" />
             <div>
-                <button id='vouched-button'>VERIFY NOW</button>
-                <div id='vouched-root' className="full-width"></div>
+                <button id='vouched-button' onClick={toggleVouched}>VERIFY NOW</button>
+                {showVouchedRoot ? <div id='vouched-root' className="full-width"></div> : null}
             </div>
         </div>
     );
 
 }
 
-export default Vouched;
+export default VouchedProvider;
--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
         <Layout>
           <Script src="https://cdn.veriff.me/sdk/js/1.1/veriff.min.js" strategy="beforeInteractive"/>
           <Script src="https://cdn.veriff.me/incontext/js/v1/veriff.js" strategy="beforeInteractive" />
+          <Script src="https://static.vouched.id/widget/vouched-2.0.0.js" strategy="beforeInteractive" />
         <Component {...pageProps} />
         </Layout>
     </>
