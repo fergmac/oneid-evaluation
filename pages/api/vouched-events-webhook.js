277d38e9d6a9458685b7a68c7be2e9c58eb8e627
Fergus MacConnell <f.macconnell@gmail.com>
2022-04-21 16:48:25 -0700

    Set up Vouched cloud functions and webhook.

--- pages/api/vouched-events-webhook.js
+++ pages/api/vouched-events-webhook.js
@@ -1,9 +1,27 @@
 async function handler(req, res) {
-    const providerResponse = req.body;
+    const providerResponse = JSON.parse(req.body);
+    const data = {
+        "user_id": providerResponse.userId,
+        "session_id": providerResponse.sessionId,
+        "response": "",
+        "provider": "vouched",
+        "session_start_time": providerResponse.sessionStartTime,
+        "session_end_time": providerResponse.sessionEndTime,
+        "session_response_time": providerResponse.sessionResponseTime
+    }
+
     try {
-        console.log("Provider Response: ", providerResponse);
+        const response = await fetch("https://dcwsy6m8yg.execute-api.ca-central-1.amazonaws.com/default/one_id_testing_responses", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": process.env.API_KEY,
+            },
+            body: JSON.stringify(data)
+        });
+        res.status(response.status).json({msg: "Event Webhook Success"});
     } catch (error) {
-        console.log("Error: ", error);
+        res.status(error.status).json({msn: "Event Webhook Error"})
     }
 }
 
--- pages/api/vouched-response-webhook.js
+++ pages/api/vouched-response-webhook.js
@@ -1,9 +1,30 @@
 async function handler(req, res) {
     const providerResponse = req.body;
+
+    console.log("Vouched Webhook Response: ", providerResponse);
+    
+    const data = {
+        "user_id": providerResponse?.properties[0].userId,
+        "session_id": providerResponse?.id,
+        "response": providerResponse,
+        "provider": "vouched",
+        "session_start_time": "",
+        "session_end_time": "",
+        "session_response_time": "test-session-response-time",
+    }
+    
     try {
-        console.log("Provider Response: ", providerResponse);
+        const response = await fetch("https://dcwsy6m8yg.execute-api.ca-central-1.amazonaws.com/default/one_id_testing_responses", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": process.env.API_KEY,
+            },
+            body: JSON.stringify(data)
+        });
+        res.status(response.status).json({msg: "Decision Webhook Success"});
     } catch (error) {
-        console.log("Error: ", error);
+        res.status(error.status).json({msn: "Decision Webhook Error"})
     }
 }
 
