4545a15beaeeeec0a37c20739615ebfcc2cb6ed1
Fergus MacConnell <f.macconnell@gmail.com>
2022-04-25 15:19:47 -0700

    Add response to jumio response webhook handler.

--- pages/api/jumio-response-webhook.js
+++ pages/api/jumio-response-webhook.js
@@ -2,8 +2,10 @@ async function handler(req, res) {
     const providerResponse = req.body
     try {
         console.log("Jumio Provider Response: ", providerResponse);
+        res.status(200).json({ msg: 'Jumio Response Webhook.'}); 
     } catch (error) {
         console.log("Jumio Error: ", error);
+        res.status(error.status).json({ msg: 'Jumio Response Webhook Error.'}); 
     }
 }
 
