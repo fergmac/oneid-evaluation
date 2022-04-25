363c25cee36797dee9bac463fe29075f5fc3c95b
Fergus MacConnell <f.macconnell@gmail.com>
2022-04-25 15:12:47 -0700

    Undo parsing of jumio response.

--- pages/api/jumio-response-webhook.js
+++ pages/api/jumio-response-webhook.js
@@ -1,5 +1,5 @@
 async function handler(req, res) {
-    const providerResponse = JSON.parse(req.body);
+    const providerResponse = req.body
     try {
         console.log("Jumio Provider Response: ", providerResponse);
     } catch (error) {
