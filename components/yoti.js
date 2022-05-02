c279d99521685ec6ff3f6101a3ebf247f25d3925
rsubra13 <ramkisastra@gmail.com>
2022-05-02 16:01:30 -0700

    Yoti iframe will it work now?

--- components/yoti.js
+++ components/yoti.js
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/vouched.module.css';
 import { useRouter } from 'next/router';
@@ -6,73 +6,82 @@ import { useRouter } from 'next/router';
 function YotiProvider() {
   const origin = 'https://api.yoti.com';
   const router = useRouter();
+  const [showYotiIframe, setShowYotiIframe] = useState(false);
+  const [yotiIframeUrl, setYotiIframeUrl] = useState(new URL(process.env.NEXT_PUBLIC_YOTI_IFRAME_URL));
+
+
+  const _setYotiIframeUrl = (sessionId, clientSessionToken) => {
+    yotiIframeUrl.searchParams.set("sessionID", sessionId);
+    yotiIframeUrl.searchParams.set("sessionToken", clientSessionToken);
+    console.log("🚀 ~ file: yoti.js ~ line 36 ~ YotiProvider ~ yotiIframeUrl", yotiIframeUrl)
+    setYotiIframeUrl(yotiIframeUrl)
+  }
+
 
   useEffect(() => {
     const userData = localStorage.getItem("userData");
     !userData && router.push("/")
 
-    const iframe = document.getElementById('yoti-iframe').contentWindow;
-
-
     fetch('api/yoti-session', {
-        method: 'POST',
-        body: userData
-      }).then(res => res.json())
+      method: 'POST',
+      body: userData
+    }).then(res => res.json())
       .then(res => {
-        if (res.sessionId) {
+        if (res.sessionId && res.clientSessionToken) {
           console.log("Iframe postMessage initiated with sessionId", res.sessionId)
-          iframe.postMessage({
-            eventType: 'INIT_SESSION',
-            sessionID: res.sessionId,
-            sessionToken: res.clientSessionToken,
-          },
-            origin
-          );
+          _setYotiIframeUrl(res.sessionId, res.clientSessionToken)
+          setShowYotiIframe(true)
+        }
+        else {
+          console.error("Error:", res)
         }
       }).catch(err => console.error("Error while launching session", err))
-      window.addEventListener('message', event => {
-        if (event.data.eventType === 'STARTED' && event.origin === origin) {
-          console.log("Event started!!")
-        }
-      });
+    window.addEventListener('message', event => {
+      if (event.data.eventType === 'STARTED' && event.origin === origin) {
+        console.log("Event started!!")
+      }
+    });
 
-      window.addEventListener(
-        'message',
-        function(event) {
-          console.log('Message received', event.data);
-          if (event.data.eventType === 'SUCCESS') {
-            console.log('Success', event.data.eventType)
-          } else if (event.data.eventType === "ERROR") {
-            const errorCode = event.data.eventCode;
-            console.log("🚀 ~ file: yoti.js ~ line 45 ~ useEffect ~ errorCode", errorCode)
-          }
+    window.addEventListener(
+      'message',
+      function (event) {
+        if (event.data.eventType === 'SUCCESS') {
+          console.log('Success', event.data.eventType)
+        } else if (event.data.eventType === "ERROR") {
+          const errorCode = event.data.eventCode;
+          console.log("🚀 ~ file: yoti.js ~ line 45 ~ useEffect ~ errorCode", errorCode)
         }
-      );
+      }
+    );
 
     return function cleanup() {
       window.removeEventListener('message', event)
     }
-  },[]);
+  }, []);
 
   return (
     <div className="section">
       <Image className="logo" width="100" height="50" src="/logo_yoti.svg" alt="OneID provider logo" />
 
-        <iframe
-        src='https://api.yoti.com/idverify/v1/web/index.html'
-        loading="lazy"
-          allow="camera"
-          width="100%"
-          height="750"
-          style={{
-            height: '805px',
-            width: '100%',
-            border: 'none',
-          }}
-          id="yoti-iframe"
-          data-hj-allow-iframe=""
-          allowFullScreen
-        />
+      {showYotiIframe ?
+        (
+          <iframe
+            src={yotiIframeUrl}
+            loading="lazy"
+            allow="camera"
+            width="100%"
+            height="750"
+            style={{
+              height: '805px',
+              width: '100%',
+              border: 'none',
+            }}
+            id="yoti-iframe"
+            data-hj-allow-iframe=""
+            allowFullScreen
+          />)
+        :
+        (<label htmlFor="yoti-iframe">Loading...</label>)}
     </div>
   );
 }
