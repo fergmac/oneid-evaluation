82352cd98c6a20753fab9538e1c857823e47c4aa
rsubra13 <ramkisastra@gmail.com>
2022-05-02 12:15:37 -0700

    Yoti svg

--- components/yoti.js
+++ components/yoti.js
@@ -11,7 +11,7 @@ function YotiProvider() {
     const userData = localStorage.getItem("userData");
     !userData && router.push("/")
 
-    const iframe = document.getElementById('iframeId').contentWindow;
+    const iframe = document.getElementById('yoti-iframe').contentWindow;
 
 
     fetch('api/yoti-session', {
@@ -21,40 +21,40 @@ function YotiProvider() {
       .then(res => {
         console.log("Iframe postMessage initiated with sessionId", res.sessionId)
         iframe.postMessage({
-            eventType: 'INIT_SESSION',
-            sessionID: res.sessionId,
-            sessionToken: res.clientSessionToken,
-          },
-          origin
-        );
+          eventType: 'INIT_SESSION',
+          sessionID: res.sessionId,
+          sessionToken: res.clientSessionToken,
+        },
+        origin
+      );
       }).catch(err => console.error("Error while launching session", err))
-    window.addEventListener('message', event => {
-      if (event.data.eventType === 'STARTED' && event.origin === origin) {
-        console.log("Event started!!")
-      }
-    });
-
-    window.addEventListener(
-      'message',
-      function(event) {
-        console.log('Message received', event.data);
-        if (event.data.eventType === 'SUCCESS') {
-          console.log('Success', event.data.eventType)
-        } else if (event.data.eventType === "ERROR") {
-          const errorCode = event.data.eventCode;
-          console.log("🚀 ~ file: yoti.js ~ line 45 ~ useEffect ~ errorCode", errorCode)
+      window.addEventListener('message', event => {
+        if (event.data.eventType === 'STARTED' && event.origin === origin) {
+          console.log("Event started!!")
         }
-      }
-    );
+      });
+
+      window.addEventListener(
+        'message',
+        function(event) {
+          console.log('Message received', event.data);
+          if (event.data.eventType === 'SUCCESS') {
+            console.log('Success', event.data.eventType)
+          } else if (event.data.eventType === "ERROR") {
+            const errorCode = event.data.eventCode;
+            console.log("🚀 ~ file: yoti.js ~ line 45 ~ useEffect ~ errorCode", errorCode)
+          }
+        }
+      );
 
     return function cleanup() {
       window.removeEventListener('message', event)
     }
-  });
+  },[]);
 
   return (
     <div className="section">
-      {/* <Image className="logo" width="100" height="50" src="/logo_yoti.png" alt="OneID provider logo" /> */}
+      <Image className="logo" width="100" height="50" src="/logo_yoti.svg" alt="OneID provider logo" />
 
         <iframe
         src='https://api.yoti.com/idverify/v1/web/index.html'
@@ -67,11 +67,11 @@ function YotiProvider() {
             width: '100%',
             border: 'none',
           }}
-          id="iframeId"
+          id="yoti-iframe"
           data-hj-allow-iframe=""
           allowFullScreen
         />
-      </div>
+    </div>
   );
 }
 
