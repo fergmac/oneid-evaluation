894abe46c6c679be924bcd18d7ed7a4e44c04f7e
Fergus MacConnell <f.macconnell@gmail.com>
2022-05-05 14:23:05 -0700

    Update test.

--- pages/api/vouched-response-webhook.js
+++ pages/api/vouched-response-webhook.js
@@ -10,7 +10,7 @@ async function handler(req, res) {
     //     "session_response_time": new Date().toISOString(),
     // }
     // console.log("Vouched Response Webhook Data: ", data);
-    // try {
+    try {
     //     const response = await fetch(`${url}`, {
     //         method: "PATCH",
     //         headers: {
@@ -19,12 +19,10 @@ async function handler(req, res) {
     //         },
     //         body: JSON.stringify(data)
     //     });
-    //     res.status(200).json({msg: "Decision Webhook Success"});
-    // } catch (error) {
-    //     res.status(400).json({msn: "Decision Webhook Error"})
-    // }
-    console.log("Vouched Callback Test: ", req.body)
-    res.status(200).json({ msg: "Vouched Callback", data: req.body });
+        res.status(200).json({msg: "Decision Webhook Success", data: req?.body});
+    } catch (error) {
+        res.status(400).json({msn: "Decision Webhook Error"})
+    }
 }
 
 export default handler;
