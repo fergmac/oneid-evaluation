72e4ac286f5214fa259d42a0d461f3716ad036c0
Fergus MacConnell <f.macconnell@gmail.com>
2022-05-12 11:13:18 -0700

    Remove veriff incontext integration script.

--- pages/one-id-scripts.js
+++ pages/one-id-scripts.js
@@ -4,7 +4,7 @@ export default function OneIDScripts() {
     return (
         <>
             <Script src="https://cdn.veriff.me/sdk/js/1.1/veriff.min.js" strategy="beforeInteractive"/>
-            <Script src="https://cdn.veriff.me/incontext/js/v1/veriff.js" strategy="beforeInteractive" />
+            {/* <Script src="https://cdn.veriff.me/incontext/js/v1/veriff.js" strategy="beforeInteractive" /> */}
             <Script src="https://static.vouched.id/widget/vouched-2.0.0.js" strategy="beforeInteractive" />
             <Script src="https://www.yoti.com/share/client/" strategy="beforeInteractive" />
         </>
